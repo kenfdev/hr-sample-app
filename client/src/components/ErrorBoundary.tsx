9cacc6c944510858c77dfb3368d83f0540fb1960
Ken Fukuyama <kenfdev@gmail.com>
2022-04-25 21:49:39 +0900

    redirect to login when error

--- client/src/components/ErrorBoundary.tsx
+++ client/src/components/ErrorBoundary.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class ErrorBoundary extends React.Component {
   componentDidCatch(error: any, errorInfo: any) {
     console.log(error, errorInfo);
-    // window.location.href = '/login';
+    window.location.href = '/login';
   }
 
   render() {
